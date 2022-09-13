888b67abec6ba712a2de68832417ae52ed9e8017
suharshirajapaksha <73971127+suharshirajapaksha@users.noreply.github.com>
2022-09-13 12:22:59 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+
+  CurrentYear:number = 2022;
+  CurrentMonth:number = 9;
+  CurrentDate:number = 13;
+  
+
+
+  ngOnInit(): void{
+
+  }
 
 }
